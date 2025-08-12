236cc28b941de74071fb9af561f421a290d4573d
Darwin gomez <dsgomez@unimayor.edu.co>
2025-08-12 12:13:15 -0500

    imagenes en la nube

--- Backend/middleware/Multer.js
+++ Backend/middleware/Multer.js
@@ -1,13 +1,36 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-  }
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     const ext = path.extname(file.originalname);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+//   }
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
+
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const { v2: cloudinary } = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "procedimientos",
+    allowed_formats: ["jpg", "png", "jpeg"],
+    public_id: (req, file) => file.fieldname + '-' + Date.now(),
+  },
 });
 
 const upload = multer({ storage });
